36d2083a89b0d3b052994a889938de1a04549981
eddiesosera <21100419@virtualwindow.co.za>
2023-06-25 16:57:21 +0200

    Home Design Done

--- app/component_crud/appointment/read_appointment.php
+++ app/component_crud/appointment/read_appointment.php
@@ -1,11 +1,21 @@
+<link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Archivo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Hanken+Grotesk:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Nunito:wght@200;300;400;500;600;700;800;900;1000&family=Poppins:wght@200;300;400;500;600;700;800;900&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+</style>
+<style>
+<?php include '../../style/home.css';
+?>
+</style>
+
 <?php
+
 
 include "config.php";
 
-echo "<ul>";
+echo '<ul class="appoinment_itemWrap">';
 
 $query = "
-select session.*, dctr.doctor_name, dctr.doctor_surname, recptn.receptionist_name, ptnt.patient_name
+select session.*, dctr.doctor_name, dctr.doctor_surname, recptn.receptionist_name,recptn.receptionist_surname, ptnt.patient_name, ptnt.patient_surname
 from therapysession session, doctors dctr, receptionist recptn, patient ptnt
 where session.receptionist_id=recptn.receptionist_id and session.doctor_id=dctr.doctor_id and session.patient_id=ptnt.patient_id";
 
@@ -13,17 +23,17 @@ $result = mysqli_query($con, $query);
 while ($appoint = mysqli_fetch_assoc($result)) {
 
         echo '<li class="appoinment_item">';
-        echo '<form class="form-inline m-2" action="update.php" method="POST">';
+        echo '<form class="appoinment_item_inner" class="form-inline m-2" action="update.php" method="POST">';
 
-        echo '<div>' . $appoint['session_date'] . '</div>';
-        echo '<div>' . "Dr. " . $appoint['doctor_name'] . " " . $appoint['doctor_surname'] . '</div>';
-        echo '<div>' . $appoint['receptionist_name'] . '</div>';
-        echo '<div>' . $appoint['patient_name'] . '</div>';
-        echo '<div>' . $appoint['session_time'] . '</div>';
-        echo '<div>' . $appoint['session_room'] . '</div>';
-        echo '<div>' . $appoint['attended'] . '</div>';
+        echo '<div class"appointment_session_time" style="border:0.5px solid #1B2423;color:#1B2423; padding:10px;border-radius:8px" >' . $appoint['session_time'] . '</div>';
+        // echo '<div>' . $appoint['session_date'] . '</div>';
+        $app_itm_each = "display:flex;align-items: center;";
+        echo '<div style="' . $app_itm_each . '" class"appointment_patient_wrap">' . $appoint['patient_name'] . " " . $appoint['patient_surname'] . '</div>';
+        echo '<div style="' . $app_itm_each . '" class"appointment_doctor_wrap">' . "Dr. " . $appoint['doctor_surname'] . " " . substr(trim($appoint['doctor_name']), 0, 1)  . '</div>';
+        echo '<div style="' . $app_itm_each . '" class"appointment_receptionist_wrap">' . $appoint['receptionist_surname'] . " " . substr(trim($appoint['receptionist_name']), 0, 1) . '</div>';
+        echo '<div style="' . $app_itm_each . '" class"appointment_session_room">' . $appoint['session_room'] . '</div>';
+        echo '<div style="' . $app_itm_each . '" class"appointment_attended">' . $appoint['attended'] . '</div>';
 
-        echo '<a class="btn btn-danger" href="./component_crud/appointment/delete_appointment.php?id=' . $appoint['therapySession_id'] . '" role="button">Delete</a>';
 
         // URL variable declaration sent to edit page
         $btn_class = "appointment_edit_btn";
@@ -41,10 +51,12 @@ while ($appoint = mysqli_fetch_assoc($result)) {
         $edit_href_attnd = "&attended=" . urldecode($appoint['attended']);
 
         // URL variable sent to Appointment Edit page
-        echo '<a class="' . $btn_class . '"';
+        echo '<a style="' . $app_itm_each . '" class="secondary_btn_actn ' . $btn_class . '"';
         echo 'href="' . $edit_href_begin . $edit_href_id . $edit_href_rec . $edit_href_rec_id . $edit_href_doc . $edit_href_doc_id . $edit_href_pat . $edit_href_pat_id;
         echo $edit_href_date . $edit_href_time . $edit_href_room . $edit_href_attnd;
         echo '"' . '>Edit</a>';
+
+        echo '<a style="' . $app_itm_each . '" class="secondary_btn_dngr btn-danger" href="./component_crud/appointment/delete_appointment.php?id=' . $appoint['therapySession_id'] . '" role="button">Delete</a>';
 
 
         echo '</form>';
--- app/home.php
+++ app/home.php
@@ -1,11 +1,11 @@
 <head>
     <link href="https://cdn.jsdelivr.net/npm/remixicon@3.2.0/fonts/remixicon.css" rel="stylesheet">
     <style>
-        @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Archivo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Hanken+Grotesk:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Nunito:wght@200;300;400;500;600;700;800;900;1000&family=Poppins:wght@200;300;400;500;600;700;800;900&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Archivo+Black&family=Archivo+Narrow:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&family=Archivo:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Hanken+Grotesk:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Nunito:wght@200;300;400;500;600;700;800;900;1000&family=Poppins:wght@200;300;400;500;600;700;800;900&family=Roboto+Condensed:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Roboto+Mono:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;1,100;1,200;1,300;1,400;1,500;1,600;1,700&display=swap');
     </style>
     <style>
-        <?php include './style/home.css';
-        ?>
+    <?php include './style/home.css';
+    ?>
     </style>
 </head>
 
@@ -15,29 +15,32 @@
     <div class="home_content-wrap">
         <div class="home_content_top-wrap">
             <img class="home_content_top-imgbg" src="img/assets/background_banner2.jpg" alt="Quote Image Background" />
-            <p class="home_content_top-quoteText"> It always seems impossible until it’s done. You’ve got this Pamela
-            </p>
+            <div class="home_content_top-quoteText">
+                <p class="quoteText">
+                    It always seems impossible until it’s done. You’ve got this Pamela.
+                </p>
+            </div>
             <div class="home_content_top_interaction-wrap">
                 <div class="home_content_top_interaction-search-wrap">
-                    <i class="ri-search-2-line"></i>
-                    <input type="search" placeholder="Search session details" />
+                    <i class="searchIcon ri-search-2-line"></i>
+                    <input id="searchInput" type="search" placeholder="Search session details" />
                 </div>
                 <a href="component_ui/appointments/new_appointment.php">
                     <button class="home_content_top_interaction-newAppointment-wrap">
-                        <i class="ri-add-line home_content_top_interaction-newAppointment-icon"></i>
+                        <i class="addBtn ri-add-line home_content_top_interaction-newAppointment-icon"></i>
                         <div class="home_content_top_interaction-newAppointment-text">Appointment</div>
                     </button>
                 </a>
             </div>
         </div>
 
-        <di class="appointments_wrap">
+        <div class="appointments_wrap">
             <div class="appointments_weekly_dates-wrap">
                 <?php
 
 
                 for ($x = 0; $x <= 6; $x++) {
-                    echo "<div class='weekly_date-itm' id='weekly_date-itm$x'>";
+                    echo "<div class='weekly_date-itm  selected_weekly_date-itm$x' id='selected_weekly_date weekly_date-itm$x'>";
                     $startdate = strtotime(" $x days");
                     echo "<div class='dates-day' id='date-day$x'>" . date("D", $startdate) . "</div>";
                     echo "<div class='dates' id='date$x'>" . date("M d", $startdate) . "</div>";
@@ -55,19 +58,19 @@
                 ?>
             </div>
 
-        </di>
+        </div>
     </div>
 
     <script type="text/javascript">
-        for (let i = 0; i <= 6; i++) {
-            const date = document.querySelector("#date" + i)
-            date.addEventListener("click", wave)
+    for (let i = 0; i <= 6; i++) {
+        const date = document.querySelector("#date" + i)
+        date.addEventListener("click", wave)
 
-            async function wave(e) {
-                // alert(e.target.id)
-                document.querySelector("#date" + i).innerHTML = "YOU CLICKED ME!" + i;
-            }
+        async function wave(e) {
+            // alert(e.target.id)
+            document.querySelector("#date" + i).innerHTML = "YOU CLICKED ME!" + i;
         }
+    }
     </script>
 
 </body>
